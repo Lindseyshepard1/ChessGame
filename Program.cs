36f39adf283dc3f2cd61b419bf686c087c217593
Lindsey Shepard <52532740+lindseyshepard@users.noreply.github.com>
2019-08-24 13:47:43 -0400

    chess game
    for and ifs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int horizontal = 8;
+            int vertical = 8;
+
+            for(int i=0; i<horizontal;i++)
+            {
+                for(int across=0; across<vertical;across++)
+                {
+                    if((i+across)%2==0)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("O");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
